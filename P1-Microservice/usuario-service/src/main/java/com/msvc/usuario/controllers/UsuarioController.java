cbe3ff7b67277e0188c072ce36c35b3bb8db3f32
venomCrack98 <secflom210@hotmail.com>
2024-06-16 17:09:33 -0500

    [FEAT] se agregar reslicence4j para la excepcion de errores al momento que un servicio no responda

--- P1-Microservice/usuario-service/src/main/java/com/msvc/usuario/controllers/UsuarioController.java
+++ P1-Microservice/usuario-service/src/main/java/com/msvc/usuario/controllers/UsuarioController.java
@@ -2,12 +2,18 @@ package com.msvc.usuario.controllers;
 
 import com.msvc.usuario.entities.Usuario;
 import com.msvc.usuario.service.UsuarioService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.UUID;
 
+
+@Slf4j
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
@@ -19,14 +25,27 @@ public class UsuarioController {
         Usuario usuario =service.saveUsuario(usuarioRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
     }
-    @GetMapping("/{usuarioid}")
-    public ResponseEntity<Usuario> obtenerUsuario(@PathVariable String usuarioid){
-        Usuario usuario= service.getUsuario(usuarioid);
-        return ResponseEntity.ok(usuario);
-    }
+
     @GetMapping
     public ResponseEntity<List<Usuario>> listarUsuario(){
         List<Usuario> usuarios = service.getAllUsuario();
         return ResponseEntity.ok(usuarios);
+    }
+
+    @GetMapping("/{usuarioid}")
+    @CircuitBreaker(name ="ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    public ResponseEntity<Usuario> obtenerUsuario(@PathVariable String usuarioid){
+        Usuario usuario= service.getUsuario(usuarioid);
+        return ResponseEntity.ok(usuario);
+    }
+    public ResponseEntity<Usuario> ratingHotelFallback(String usuarioId, Exception exception){
+        log.info("El  respaldo se ejecuta porque el servicio esta inactivo");
+        Usuario usuario = Usuario.builder()
+                .email("root@Gmail.com")
+                .nombre("root")
+                .informacion("Este usuario se crea por defecto cuando un servicio se cae")
+                .usuarioId("1234")
+                .build();
+        return new ResponseEntity<>(usuario, HttpStatus.OK);
     }
 }
--- P1-Microservice/usuario-service/src/main/java/com/msvc/usuario/entities/Usuario.java
+++ P1-Microservice/usuario-service/src/main/java/com/msvc/usuario/entities/Usuario.java
@@ -1,10 +1,7 @@
 package com.msvc.usuario.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 @Table(name = "usuarios")
 public class Usuario {
 
