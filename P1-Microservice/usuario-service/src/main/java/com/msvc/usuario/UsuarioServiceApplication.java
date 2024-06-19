07bc39444ce4bea62bc97f0e23768ca23bd0a3db
venomCrack98 <secflom210@hotmail.com>
2024-06-18 22:27:49 -0500

    [FEAT] actulizacion de eureka client en las clases principales, y agregar msc de autentificacion solo base

--- P1-Microservice/usuario-service/src/main/java/com/msvc/usuario/UsuarioServiceApplication.java
+++ P1-Microservice/usuario-service/src/main/java/com/msvc/usuario/UsuarioServiceApplication.java
@@ -2,13 +2,15 @@ package com.msvc.usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
- * @EnableEurekaClient no es ncesario por la version de spring, por tal motivo
+ * EnableEurekaClient no es ncesario por la version de spring, por tal motivo
  * no se coloca y el servicio funciona normal
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 public class UsuarioServiceApplication {
 
