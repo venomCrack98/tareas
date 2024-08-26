082a9904e5e414bd32f2507721b309399ae90079
venomCrack98 <secflom210@hotmail.com>
2024-08-25 22:23:28 -0500

    [FIX] se devuelve cambio de klickloa

--- Proyecto02/msv-SB/api-gateway/src/main/java/com/api/gateway/config/SecurityConfig.java
+++ Proyecto02/msv-SB/api-gateway/src/main/java/com/api/gateway/config/SecurityConfig.java
@@ -1,15 +1,15 @@
 package com.api.gateway.config;
 
-import org.springframework.context.annotation.Bean;
+/*import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;*
 
 
-@Configuration
-@EnableWebFluxSecurity
+//@Configuration
+//@EnableWebFluxSecurity
 public class SecurityConfig {
   /*  @Bean
     public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
@@ -24,7 +24,7 @@ public class SecurityConfig {
         return serverHttpSecurity.build();
     }
 */
-    @Bean public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+    /*@Bean public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity.csrf(csrf -> csrf.disable())
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/eureka/**")
@@ -34,5 +34,5 @@ public class SecurityConfig {
                 .oauth2ResourceServer((oauth2) -> oauth2
                         .jwt(Customizer.withDefaults()));
         return serverHttpSecurity.build();
-    }
-}
+    }*/
+//}
--- Proyecto02/msv-SB/discovery-server/src/main/java/com/discovery/eureka/config/SecurityConfig.java
+++ Proyecto02/msv-SB/discovery-server/src/main/java/com/discovery/eureka/config/SecurityConfig.java
@@ -1,14 +1,14 @@
 package com.discovery.eureka.config;
 
-import org.springframework.beans.factory.annotation.Value;
+/*import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;*/
 
-@Configuration
+/*@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${app.eureka.username}")
     private String username;
@@ -31,4 +31,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().httpBasic();
         super.configure(http);
     }
-}
+}*/
